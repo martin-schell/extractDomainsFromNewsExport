36bdb8d14c19c6d717943bbf21c829f0d152e0e6
Martin Schell <fredeulennest@gmail.com>
2025-06-08 15:35:04 +0200

    Added in Script: Parameter for output file, checks and removing duplicates in out_file

--- extractDomainsFromNewsExport.sh
+++ extractDomainsFromNewsExport.sh
@@ -8,12 +8,12 @@
 # Log resolving into a log file
 
 SCRIPTNAME=$(basename "$0" | cut -d'.' -f1)
-OUT_FILE="$SCRIPTNAME".out
 LOG_FILE="$SCRIPTNAME".log
 
 usage() {
   echo "Usage: $0"
   echo "-i, --input <file>  Read OPML export from Nextcloud News"
+  echo "-o, --output <file> Output file"
   echo "-h, --help          Print usage"
 }
 
@@ -26,11 +26,39 @@ log() {
     echo "$TIMESTAMP [$LEVEL] $MESSAGE" >> "$LOG_FILE"
 }
 
+# Check if no parameters were passed
+if [ $# -eq 0 ]; then
+    echo "Error: No arguments provided."
+    usage
+    exit 1
+fi
+
+# Defaults
+out_file="$SCRIPTNAME".out
+
 # Parse arguments
 while [[ $# -gt 0 ]]; do
   case "$1" in
     -i | --input)
+      if [[ -z "$2" || "$2" == --* ]]; then
+        echo "Error: -i, --input requires a value."
+        exit 1
+		  elif [[ ! -f "$2" ]]; then
+			  echo "ERROR" "$2 does not exist"
+			  exit 1
+      elif [[ -z "$(ls -A "$2")" ]]; then
+        echo "ERROR" "$2 is emtpy"
+        exit 1
+      fi
       in_file="$2"
+      shift 2
+      ;;
+    -o | --output)
+      if [[ -z "$2" || "$2" == --* ]]; then
+        echo "Error: -i, --input requires a value."
+        exit 1
+      fi
+      out_file="$2"
       shift 2
       ;;
     -h | --help)
@@ -46,7 +74,7 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Clear the output files
-echo "" > "$OUT_FILE"
+echo "" > "$out_file"
 # Clear the log file
 echo "" > "$LOG_FILE"
 
@@ -56,8 +84,10 @@ domains=$(grep -o 'xmlUrl="https\{0,1\}://[^/"]*' "$in_file" | sed 's/xmlUrl="//
 # Collect annotated results into a temp file
 for domain in $domains; do
   log "DEBUG" "Add $domain"
-  echo "$domain" >> "$OUT_FILE"
+  echo "$domain" >> "$out_file"
 done
 
 # Remove empty line
-sed -i '/^$/d' "$OUT_FILE"
+sed -i '/^$/d' "$out_file"
+# Remove duplicates
+sort -u -o "$out_file" "$out_file"
